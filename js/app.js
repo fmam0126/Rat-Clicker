5050a9a828ce5c634d485f5f64f9d40bdead913f
fmam0126 <fmam012026@gmail.com>
2026-01-29 14:09:17 +0100

    Refactor HTML structure and improve CSS layout for buy section; update JavaScript to use textContent for better performance

--- js/app.js
+++ js/app.js
@@ -8,12 +8,12 @@ let autoclickpower = 0
 
 function ratclick(){
     money = money + clickpower
-    moneyElement.innerText = money
+    moneyElement.textContent = money
 }
 
 function autoclicker(){
     money = money + autoclickpower
-    moneyElement.innerText = money
+    moneyElement.textContent = money
 }
 
 setInterval(() => {
@@ -28,17 +28,34 @@ function buyclickpower1(){
     if (money >= 10){
         money = money - 10
         clickpower = clickpower + 1
-        moneyElement.innerText = money  
+        moneyElement.textContent = money  
     }else{
         alert("you have no money!")
     }
 
 }
+function buyallclickpower1(){
+    let boughtSomething = false;
+    while (money >= 10){
+        money -= 10
+        clickpower += 1
+        boughtSomething = true
+    }
+    if (boughtSomething){
+        console.log(boughtSomething);
+        
+        moneyElement.textContent = money
+    } else {
+        console.log(boughtSomething)
+        alert("you have no money!")
+    }
+}
+
 function buyclickpower10(){
     if (money >= 100){
         money = money - 100
         clickpower = clickpower + 10
-        moneyElement.innerText = money  
+        moneyElement.textContent = money  
     }else{
         alert("you have no money!")
     }
@@ -48,7 +65,7 @@ function buyautoclicker1(){
     if (money >= 200){
         money = money - 200
         autoclickpower = autoclickpower + 1
-        moneyElement.innerText = money  
+        moneyElement.textContent = money  
     }else{
         alert("you have no money!")
     }
